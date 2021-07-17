60a3b91fc09a8d92fdc01376f6b0620c55fa8935
Benji Wiebe <benjiwiebe14@gmail.com>
2021-07-16 22:36:13 -0500

    Some changes, IDK

--- background.js
+++ background.js
@@ -1,6 +1,7 @@
 'use strict';
 
-var canvas, tabcount = 0, debugging = false;
+var canvas, tabcount = 0, debugging = false, icon_ctx, icon_size = 32;
+var font = "28px caption";
 
 function oops(error) {
 	setCount("!");
@@ -27,12 +28,15 @@ function setCount(num) {
 	canvas.height = 30;
 	canvas.width = msg.length * 18;
 	var ctx = canvas.getContext("2d");
-	ctx.font = "30px Courier";
+	ctx.font = font;
 	ctx.textBaseline = "top";
 	ctx.fillText(msg,0,0);
 	var imgdata = ctx.getImageData(0,0,canvas.width,canvas.height);
 	if(!debugging) {
 		browser.browserAction.setIcon({imageData:imgdata});
+	} else {
+		icon_ctx.clearRect(0,0,icon_size,icon_size);
+		icon_ctx.putImageData(imgdata, 0, 0);
 	}
 }
 
@@ -40,8 +44,11 @@ if(typeof browser == "undefined") { // We are running in the test.html page
 	window.onload = function () {
 		debugging = true;
 		console.log("Detected debug page");
+		var icon_canvas = document.getElementById('icon');
+		icon_canvas.width = icon_size;
+		icon_canvas.height = icon_size;
+		icon_ctx = icon_canvas.getContext("2d");
 		canvas = document.getElementById('canvas');
-		canvas.style.border = "thick solid red";
 		setCount("124 Oy"); // Some numbers, a capital letter, and a letter with a tail, to check if it fits.
 	};
 } else {
